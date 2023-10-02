1e7459ca4d67fee467b3291dc6028bb16adca92e
howard <howard@rtzr.ai>
2023-10-02 17:06:07 +0900

    [기타] - Sequence + 추가.

---
+++ Sources/HowardCommon/Extensions/Sequence+.swift
@@ -0,0 +1,59 @@
+//
+//  File.swift
+//
+//
+//  Created by howard on 10/2/23.
+//
+
+import Foundation
+
+extension Sequence {
+    public func asyncMap<T>(
+        _ transform: (Element) async throws -> T
+    ) async rethrows -> [T] {
+        var values = [T]()
+        
+        for element in self {
+            try await values.append(transform(element))
+        }
+        
+        return values
+    }
+    
+    public func asyncForEach(
+        _ operation: (Element) async throws -> Void
+    ) async rethrows {
+        for element in self {
+            try await operation(element)
+        }
+    }
+    
+    public func concurrentForEach(
+        _ operation: @escaping (Element) async -> Void
+    ) async {
+        // A task group automatically waits for all of its
+        // sub-tasks to complete, while also performing those
+        // tasks in parallel:
+        await withTaskGroup(of: Void.self) { group in
+            for element in self {
+                group.addTask {
+                    await operation(element)
+                }
+            }
+        }
+    }
+    
+    public func concurrentMap<T>(
+        _ transform: @escaping (Element) async throws -> T
+    ) async throws -> [T] {
+        let tasks = map { element in
+            Task {
+                try await transform(element)
+            }
+        }
+        
+        return try await tasks.asyncMap { task in
+            try await task.value
+        }
+    }
+}
